3bef0842a1fd24b9050ec62290a99f939ac8f188
Santiago <santirpo115@gmail.com>
2023-08-26 08:59:52 -0500

    [Test] Unit Test ModalMessageComponent

--- src/app/shared/components/modal-message/modal-message.component.spec.ts
+++ src/app/shared/components/modal-message/modal-message.component.spec.ts
@@ -1,15 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ModalMessageComponent } from './modal-message.component';
+import { ModalMessageService } from '../../services/modal-message.service'; // Replace with the actual import path
+import { Subject, of } from 'rxjs';
+
 
 describe('ModalMessageComponent', () => {
   let component: ModalMessageComponent;
   let fixture: ComponentFixture<ModalMessageComponent>;
+  let mockModalMessageService: any;
+
+  beforeEach(async () => {
+    mockModalMessageService = jasmine.createSpyObj(['title', 'message', 'close']);
+    mockModalMessageService.title = of('Mock Title');
+    mockModalMessageService.message = of('Mock Message');
+    mockModalMessageService.watch = jasmine.createSpy('watch').and.returnValue(of('open'));
+
+    await TestBed.configureTestingModule({
+      declarations: [ModalMessageComponent],
+      providers: [
+        { provide: ModalMessageService, useValue: mockModalMessageService },
+      ],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ModalMessageComponent]
-    });
     fixture = TestBed.createComponent(ModalMessageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +32,20 @@ describe('ModalMessageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize title, message, and display$', () => {
+    expect(component.title).toBe('Mock Title');
+    expect(component.message).toBe('Mock Message');
+    expect(component.display$).toBeTruthy();
+  });
+
+  it('should close modal via modalMessageService', () => {
+    component.close();
+    expect(mockModalMessageService.close).toHaveBeenCalled();
+  });
+
+  afterEach(() => {
+    TestBed.resetTestingModule();
   });
 });
