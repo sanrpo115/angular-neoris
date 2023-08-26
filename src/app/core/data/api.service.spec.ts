8f5154eeab439c84e8496df6f70f3b1dc9964114
Santiago <santirpo115@gmail.com>
2023-08-26 08:15:29 -0500

    [Test] Unit Test ApiService & ProductRepository

--- src/app/core/data/api.service.spec.ts
+++ src/app/core/data/api.service.spec.ts
@@ -1,26 +1,101 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ApiService } from './api.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CommonModule } from '@angular/common';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         CommonModule,
-        HttpClientModule
+        HttpClientTestingModule
       ],
       providers: [
         ApiService
       ]
     });
     service = TestBed.inject(ApiService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should send GET request', () => {
+    const endpoint = 'test';
+    const responseData = { message: 'Test response' };
+
+    service.get(endpoint).subscribe((response) => {
+      expect(response).toEqual(responseData);
+    });
+
+    const req = httpMock.expectOne(`${service['urlBase']}/${endpoint}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(responseData);
+  });
+
+  it('should send POST request', () => {
+    const endpoint = 'test';
+    const requestData = { data: 'Test data' };
+    const responseData = { message: 'Test response' };
+
+    service.post(endpoint, requestData).subscribe((response) => {
+      expect(response.body).toEqual(responseData);
+    });
+
+    const req = httpMock.expectOne(`${service['urlBase']}/${endpoint}`);
+    expect(req.request.method).toBe('POST');
+    req.flush(responseData);
+  });
+
+  it('should send PUT request', () => {
+    const endpoint = 'test';
+    const requestData = { data: 'Test data' };
+    const responseData = { message: 'Test response' };
+
+    service.put(endpoint, requestData).subscribe((response) => {
+      expect(response.body).toEqual(responseData);
+    });
+
+    const req = httpMock.expectOne(`${service['urlBase']}/${endpoint}`);
+    expect(req.request.method).toBe('PUT');
+    req.flush(responseData);
+  });
+
+  it('should send DELETE request', () => {
+    const endpoint = 'test';
+    const responseData = { message: 'Test response' };
+
+    service.delete(endpoint).subscribe((response) => {
+      expect(response).toEqual(responseData);
+    });
+
+    const req = httpMock.expectOne(`${service['urlBase']}/${endpoint}`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush(responseData);
+  });
+
+  it('should send VERIFY request', () => {
+    const endpoint = 'test';
+    const responseData = { message: 'Test response' };
+
+    service.verify(endpoint).subscribe((response) => {
+      expect(response).toEqual(responseData);
+    });
+
+    const req = httpMock.expectOne(`${service['urlBase']}/${endpoint}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(responseData);
+  });
+
+
+
 });
--- src/app/core/infrastructure/api/product.repository.api.spec.ts
+++ src/app/core/infrastructure/api/product.repository.api.spec.ts
@@ -1,7 +1,69 @@
+import { TestBed } from '@angular/core/testing';
 import { ProductRepositoryApi } from './product.repository.api';
+import { ApiService } from '../../data/api.service';
+import { of } from 'rxjs';
+import { Product } from '../../services/domain/models/product.model';
 
-// describe('ProductRepositoryApi', () => {
-//   it('should create an instance', () => {
-//     expect(new ProductRepositoryApi()).toBeTruthy();
-//   });
-// });
+describe('ProductRepositoryApi', () => {
+  let productRepository: ProductRepositoryApi;
+  let mockApiService: any;
+  const mockData = [
+    { id: "test-5", name: "test", description: "test", logo: "test", date_release: "2023-12-12T00:00:00.000+00:00", date_revision: "2024-12-12T00:00:00.000+00:00" },
+    { id: "test-2", name: "test", description: "test", logo: "test", date_release: "2023-11-12T00:00:00.000+00:00", date_revision: "2024-11-12T00:00:00.000+00:00" }
+  ];
+
+  beforeEach(() => {
+    mockApiService = jasmine.createSpyObj(['get', 'post', 'put', 'delete', 'verify']);
+    TestBed.configureTestingModule({
+      providers: [
+        ProductRepositoryApi,
+        { provide: ApiService, useValue: mockApiService },
+      ],
+    });
+    productRepository = TestBed.inject(ProductRepositoryApi);
+  });
+
+  it('should retrieve products from ApiService', () => {
+    mockApiService.get.and.returnValue(of(mockData));
+
+    const products$ = productRepository.getProducts();
+
+    products$.subscribe((products) => {
+      expect(products).toEqual(mockData);
+    });
+  });
+
+  it('should create a product using ApiService', () => {
+    mockApiService.post.and.returnValue(of(mockData));
+    const createProduct$ = productRepository.createProduct(mockData[0]);
+
+    createProduct$.subscribe((response) => {
+      expect(response).toEqual(mockData);
+    });
+  });
+
+  it('should update a product using ApiService', () => {
+    const mockProduct: Product = mockData[0];
+    mockApiService.put.and.returnValue(of(mockProduct));
+    const updateProduct$ = productRepository.updateProduct(mockProduct);
+
+    updateProduct$.subscribe((response) => {
+      expect(response).toEqual(mockProduct);
+    });
+    expect(mockApiService.put).toHaveBeenCalledWith('products', mockProduct);
+  });
+
+  it('should verify id using ApiService', () => {
+    const mockId = '123';
+    const mockResponse = true;
+    mockApiService.verify.and.returnValue(of(mockResponse));
+
+    const verification$ = productRepository.verificationId(mockId);
+
+    verification$.subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+      expect(mockApiService.verify).toHaveBeenCalledWith(`products/verification?id=${mockId}`);
+    });
+  });
+
+});
